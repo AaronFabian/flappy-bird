2455b6750ad8ed8abcfa996205a0cd70be7b859d
AaronFabian <aaronfabian78@gmail.com>
2022-11-23 20:25:13 +0900

    fixing sound-2

---
+++ dist/index.3652531f.js
@@ -0,0 +1,2 @@
+const audio={backgroundMusic:new Howl({src:["./audio/marios_way.mp3"],loop:!0,volume:.2}),jump:new Howl({src:["./audio/jump.wav"],volume:.1}),scoring:new Howl({src:["./audio/score.wav"],volume:.1}),lost:new Howl({src:["./audio/hurt.wav"],volume:.1})};
+//# sourceMappingURL=index.3652531f.js.map
