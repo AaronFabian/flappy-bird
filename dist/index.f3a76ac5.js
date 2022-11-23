38386b64c292c7a11345aa2fd4a8eba2a55b505a
AaronFabian <aaronfabian78@gmail.com>
2022-11-23 20:06:09 +0900

    fixing sound-2

---
+++ dist/index.f3a76ac5.js
@@ -0,0 +1,2 @@
+const audio={backgroundMusic:new Howl({src:["./audio/marios_way.mp3"]}),jump:new Howl({src:["./audio/jump.wav"]}),scoring:new Howl({src:["./audio/score.wav"]}),lost:new Howl({src:["./audio/hurt.wav"]})};
+//# sourceMappingURL=index.f3a76ac5.js.map
